f4819b7f6478f3515b52b0a5b5ce2620fab1e97d
alvin joseph valdez <alvinjosephvaldez@alvins-macbook-pro.local>
2019-01-08 22:14:30 +0800

    Initial Commit

---
+++ freeHeroTests/freeHeroTests.swift
@@ -0,0 +1,34 @@
+//
+//  freeHeroTests.swift
+//  freeHeroTests
+//
+//  Created by alvin joseph valdez on 08/01/2019.
+//  Copyright © 2019 alvin joseph valdez. All rights reserved.
+//
+
+import XCTest
+@testable import freeHero
+
+class freeHeroTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
